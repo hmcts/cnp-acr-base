71727dfeb815cd4a119dcf853d2c1befae5731f8
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-06-25 13:07:52 +0100

    Re-order vars

--- tag-new-image.sh
+++ tag-new-image.sh
@@ -7,8 +7,8 @@ baseTag=$1
 targetImage=$2
 acrName=$3
 targetRegistry=$4
-acrDigest=$5
-baseDigest=$6
+baseDigest=$5
+acrDigest=$6
 
 # Move base tag to new image
 [ "${acrDigest}" != "" ] && echo "Untagging previous ${baseTag} ..." && az acr repository untag -n ${acrName} --image ${targetImage}:${baseTag}
