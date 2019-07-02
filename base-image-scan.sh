ccf7bd4ffd45344e935a6dc8c15bf191980d7693
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-07-02 10:06:23 +0100

    Add logging

--- base-image-scan.sh
+++ base-image-scan.sh
@@ -12,15 +12,14 @@ scannerUrl=$7
 scannerUsername=$8
 scannerPassword=$9
 
-# test only. to remove after first run
-curl -v -k -H "Content-Type: application/json" -d "{\"password\": {\"username\": \"${scannerUsername}\", \"password\": \"invalid\"}}" \
-  "${scannerUrl}/v1/auth"
 
+echo "Getting auth token ..."
 _token=$(curl -s -k -H "Content-Type: application/json" -d "{\"password\": {\"username\": \"${scannerUsername}\", \"password\": \"${scannerPassword}\"}}" \
   "${scannerUrl}/v1/auth" | jq .token.token |sed 's/"//g')
 
 [[ "${_token}" == "" ]] && echo "Authentication to image scanner failed." && exit 1
 
+echo "Scanning image ${targetImage} ..."
 _scan_results=$(curl -v -k -H 'Content-Type: application/json' -H "X-Auth-Token: $_token" \
   -d "{\"request\":{\"registry\":\"https://${targetRegistry}\",\"repository\":\"${targetImage}\",\"tag\":\"${baseTag}\",\"username\":\"${registryPublicUsername}\",\"password\":\"${registryPublicPassword}\"}}" \
   "${scannerUrl}/v1/scan/repository") 
@@ -39,6 +38,7 @@ then
 else
   _msg_text="Scanned new image ${targetRegistry}/${targetImage}:${baseTag}. No high or critical severity vulnerabilities found."
 fi
+echo "Scan summary: [${_msg_text}]"
 
 curl -X POST --data-urlencode "payload={\"channel\": \"#acr-tasks-monitoring\", \"username\": \"NeuVector\", \"text\": \"${_msg_text}\", \"icon_emoji\": \":liam_is_watching:\"}" \
   ${registrySlackWebhook}
