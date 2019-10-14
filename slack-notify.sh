36481fffd1853b334017a869567da35e318fb02f
Luigi Bitonti <luigi.bitonti@hmcts.net>
2019-10-14 09:24:21 +0100

    Icon as script parameter

--- slack-notify.sh
+++ slack-notify.sh
@@ -4,7 +4,8 @@ set -e
 
 slackMessage=$1
 slackWebhook=$2
+slackIcon=${3:-tim-webster}
 
-curl -X POST --data-urlencode "payload={\"channel\": \"#acr-tasks-monitoring\", \"username\": \"NeuVector\", \"text\": \"${slackMessage}\", \"icon_emoji\": \":tim-webster:\"}" \
+curl -X POST --data-urlencode "payload={\"channel\": \"#acr-tasks-monitoring\", \"username\": \"NeuVector\", \"text\": \"${slackMessage}\", \"icon_emoji\": \":${slackIcon}:\"}" \
   ${slackWebhook}
 
