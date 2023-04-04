aa54a475313fb7fa9f85b0b2d9102d49f907ba4c
Tim Jacomb <21194782+timja@users.noreply.github.com>
2023-04-04 15:13:09 +0100

    Importer tweaks (#46)

--- check-base-tag.sh
+++ check-base-tag.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
@@ -6,29 +6,25 @@ baseImage=$1
 baseRegistry=$2
 baseTag=$3
 targetImage=$4
-baseImageType=$5
-acrName=$6
+acrName=$5
 
-# Get digest for tag from base registry (e.g. docker)
-if [ ${baseImageType} == "docker" ]
+_result=$(docker buildx imagetools inspect --raw $baseRegistry/$baseImage:$baseTag)
+
+if echo $_result | grep -q manifests
 then
-  # docker registry
-  _token=$(curl --silent "https://auth.docker.io/token?scope=repository:${baseImage}:pull&service=registry.docker.io" | jq -r '.token')
-  _digest=$(curl -i --silent --header "Accept: application/vnd.docker.distribution.manifest.v2+json" --header "Authorization: Bearer $_token" \
-    "https://registry-1.docker.io/v2/${baseImage}/manifests/${baseTag}" |grep -i '[Dd]ocker-[Cc]ontent-[Dd]igest:' \
-    |sed 's/[Dd]ocker-[Cc]ontent-[Dd]igest: *\(sha256:[a-zA-Z0-9]*\)/\1/' |tr -d '\r\n')
+  _digest=$(echo $_result | jq -r '.manifests[] | select (.platform.architecture == "amd64") | .digest')
 else
-  # google registry (gcr)
-  _digest=$(curl -i --silent "https://gcr.io/v2/${baseImage}/manifests/${baseTag}" |grep -i '[Dd]ocker-[Cc]ontent-[Dd]igest:' \
-    |sed 's/[Dd]ocker-[Cc]ontent-[Dd]igest: *\(sha256:[a-zA-Z0-9]*\)/\1/' |tr -d '\r\n')
+  _digest=$(echo $_result | jq -r .config.digest)
 fi
 
 [ "$_digest" == "" ] && echo "Error: cannot get image digest for ${baseImage}:${baseTag}" && exit 1
 
 # Get current digest from target azure registry
 echo "Base registry current digest for ${baseImage}:${baseTag}: [${_digest}]"
-_acr_digest=$(az acr repository show-manifests -n ${acrName} --repository ${targetImage} \
-  --query "[?not_null(tags[])]|[?contains(tags, \`\"${baseTag}\"\`)].digest|[0]" |tr -d '"[:blank:]' |xargs echo -n)
+
+_acr_digest=$(az acr manifest list-metadata --registry $acrName --name $targetImage \
+ --query "[?not_null(tags[])]|[?contains(tags, \`\"${baseTag}\"\`)].digest|[0]" -o tsv)
+
 echo "Target registry current digest for ${baseImage}:${baseTag}: [${_acr_digest}]"
 
 [[ "$_acr_digest" != "" && "$_acr_digest" == "$_digest" ]] && echo "Nothing to import for ${baseRegistry}/${baseImage}." && exit 0  # Nothing else to do
