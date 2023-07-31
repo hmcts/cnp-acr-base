73151ea65ebe4a952077895208ba3c8401f70772
Vik <viktoriya.bogdanova@hmcts.net>
2023-07-31 12:35:02 +0100

    test acr cache

--- acr-cache.sh
+++ acr-cache.sh
@@ -20,16 +20,16 @@ echo $tagVersion
 # systemctl start docker
 
 echo "Logging into ACR..."
-az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --expose-token
+az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1
 
-az account set —subscription DCD-CNP-Prod
+az account set --subscription DCD-CNP-Prod
 az acr credential-set create -r hmctspublic.azurecr.io -n credentials -l docker.io -u https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-username -p https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-password
 
 az acr cache create -r hmctspublic.azurecr.io -n $ruleName -s docker.io/$repoName -t $destinationRepo -c credentials
 
 PRINCIPAL_ID=$(az acr credential-set show -n credentials  -r hmctspublic.azurecr.io  --query 'identity.principalId'  -o tsv)
 
-az account set —subscription DTS-CFTPTL-INTSVC
+az account set --subscription DTS-CFTPTL-INTSVC
 az keyvault set-policy --name cftptl-intsvc --object-id $PRINCIPAL_ID --secret-permissions get
 
 docker pull hmctspublic.azurecr.io/$destinationRepo:$tagVersion
