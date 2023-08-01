17c0b65d7f7ae51b044667c4c75c016b679981fd
Vik <viktoriya.bogdanova@hmcts.net>
2023-08-01 14:57:55 +0100

    test acr cache

--- acr-cache.sh
+++ acr-cache.sh
@@ -6,7 +6,15 @@ echo "Logging into ACR..."
 az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --expose-token
 
 echo "Create ACR Credentials"
-az acr credential-set create -r hmctspublic -n credentials -l docker.io -u https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-username -p https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-password
+az acr credential-set create -r hmctspublic -n credentials1 -l docker.io -u https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-username -p https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-password
+
+PRINCIPAL_ID=$(az acr credential-set show -n credentials1  -r hmctspublic  --query 'identity.principalId'  -o tsv)
+
+echo "Setup Subscription DTS-CFTPTL-INTSVC"
+az account set --subscription DTS-CFTPTL-INTSVC
+
+echo "Create KV Policy"
+az keyvault set-policy --name "cftptl-intsvc" --object-id $PRINCIPAL_ID --secret-permissions get
 
 RULES_CONFIG=$(yq e acr-repositories.yaml -o=json)
 # echo $RULES_CONFIG
@@ -22,17 +30,9 @@ for key in $(echo $RULES_CONFIG | jq -r '.rules | keys | .[]'); do
     TAG_VERSION=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .tagVersion')
     # echo "tagVersion is $TAG_VERSION"
     echo "Create ACR Cache"
-    az acr cache create -r hmctspublic -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME -c credentials
+    az acr cache create -r hmctspublic -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME -c credentials1
 
-    PRINCIPAL_ID=$(az acr credential-set show -n credentials  -r hmctspublic  --query 'identity.principalId'  -o tsv)
-
-    echo "Setup Subscription DTS-CFTPTL-INTSVC"
-    az account set --subscription DTS-CFTPTL-INTSVC
-
-    echo "Create KV Policy"
-    az keyvault set-policy --name "cftptl-intsvc" --object-id $PRINCIPAL_ID --secret-permissions get
-
-    echo "Docker Image Pull"
-    docker pull hmctspublic.azurecr.io/$DESTINATION_NAME:$TAG_VERSION
+    # echo "Docker Image Pull"
+    # docker pull hmctspublic.azurecr.io/$DESTINATION_NAME:$TAG_VERSION
 done
 
