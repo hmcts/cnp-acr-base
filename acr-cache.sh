a646bc1d9606e368fd04d58e016d0337c1506025
Vik <viktoriya.bogdanova@hmcts.net>
2023-08-01 09:37:21 +0100

    test acr cache

--- acr-cache.sh
+++ acr-cache.sh
@@ -6,15 +6,15 @@ RULES_CONFIG=$(yq e acr-repositories.yaml -o=json)
 # echo $RULES_CONFIG
 
 for key in $(echo $RULES_CONFIG | jq -r '.rules | keys | .[]'); do
-    echo "key is $key"
+    # echo "key is $key"
     RULE_NAME=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .ruleName')
-    echo "ruleName is $RULE_NAME"
+    # echo "ruleName is $RULE_NAME"
     REPO_NAME=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .repoName')
-    echo "repoName is $REPO_NAME"
+    # echo "repoName is $REPO_NAME"
     DESTINATION_NAME=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .destinationRepo')
-    echo "destinationRepo is $DESTINATION_NAME"
+    # echo "destinationRepo is $DESTINATION_NAME"
     TAG_VERSION=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .tagVersion')
-    echo "tagVersion is $TAG_VERSION"
+    # echo "tagVersion is $TAG_VERSION"
 done
 
 echo "Logging into ACR..."
@@ -24,12 +24,12 @@ az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-658855972
 # az account set --subscription DCD-CNP-Prod
 
 echo "Create ACR Credentials"
-az acr credential-set create -r hmctspublic.azurecr.io -n credentials -l docker.io -u https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-username -p https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-password
+az acr credential-set create -r hmctspublic -n credentials -l docker.io -u https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-username -p https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-password
 
 echo "Create ACR Cache"
-az acr cache create -r hmctspublic.azurecr.io -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME -c credentials
+az acr cache create -r hmctspublic -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME -c credentials
 
-PRINCIPAL_ID=$(az acr credential-set show -n credentials  -r hmctspublic.azurecr.io  --query 'identity.principalId'  -o tsv)
+PRINCIPAL_ID=$(az acr credential-set show -n credentials  -r hmctspublic  --query 'identity.principalId'  -o tsv)
 
 echo "Setup Subscription DTS-CFTPTL-INTSVC"
 az account set --subscription DTS-CFTPTL-INTSVC
