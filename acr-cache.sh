3378eb0ea72eaf0a1671124e76787770d9e8fe18
Vik <viktoriya.bogdanova@hmcts.net>
2023-08-08 13:53:57 +0100

    remove authentication

--- acr-cache.sh
+++ acr-cache.sh
@@ -2,25 +2,8 @@
 
 set -e
 
-dockerHubUsername=$1
-dockerHubPassword=$2
-
 echo "Logging into ACR..."
 az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --expose-token
-
-echo "Create ACR Credentials"
-az acr credential-set create -r hmctspublic -n credentials1 -l docker.io -u https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-username -p https://cftptl-intsvc.vault.azure.net/secrets/docker-hub-password
-
-PRINCIPAL_ID=$(az acr credential-set show -n credentials1  -r hmctspublic  --query 'identity.principalId'  -o tsv)
-
-echo "Setup Subscription DTS-CFTPTL-INTSVC"
-az account set --subscription DTS-CFTPTL-INTSVC
-
-echo "Create KV Policy"
-az keyvault set-policy --name "cftptl-intsvc" --object-id $PRINCIPAL_ID --secret-permissions get
-
-echo "Setup Subscription DCD-CNP-Prod"
-az account set --subscription DCD-CNP-Prod
 
 RULES_CONFIG=$(yq e acr-repositories.yaml -o=json)
 # echo $RULES_CONFIG
@@ -37,15 +20,7 @@ for key in $(echo $RULES_CONFIG | jq -r '.rules | keys | .[]'); do
     # echo "tagVersion is $TAG_VERSION"
 
     echo "Create ACR Cache"
-    az acr cache create -r hmctspublic -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME -c credentials1
-
-    echo "Login ${dockerHubUsername}"
-    az login --username ${dockerHubUsername} --password ${dockerHubPassword}
-
-    echo "Logging into ACR..."
-    TOKEN=$(az acr login --name hmctspublic --subscription 8999dec3-0104-4a27-94ee-6588559729d1 --expose-token)
-    docker login hmctspublic.azurecr.io -u 8999dec3-0104-4a27-94ee-6588559729d1 -p $TOKEN
-    docker login hmctspublic.azurecr.io -u "platops-docker-hub-reader@HMCTS.NET" -p $TOKEN 
+    az acr cache create -r hmctspublic -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME
 
     echo "Docker Image Pull"
     docker pull hmctspublic.azurecr.io/$DESTINATION_NAME:$TAG_VERSION
