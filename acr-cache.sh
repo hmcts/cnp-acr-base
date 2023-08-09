de560e9d4fc76a8f5606cd859d4c8483261154c4
Vik <viktoriya.bogdanova@hmcts.net>
2023-08-09 14:54:15 +0100

    update acr cache

--- acr-cache.sh
+++ acr-cache.sh
@@ -3,21 +3,15 @@
 set -e
 
 echo "Logging into ACR..."
-az acr login --name hmctspublic --subscription DCD-CNP-Prod --expose-token
+az acr login --name hmctspublic --subscription DCD-CNP-Prod
 
 RULES_CONFIG=$(yq e acr-repositories.yaml -o=json)
-# echo $RULES_CONFIG
 
 for key in $(echo $RULES_CONFIG | jq -r '.rules | keys | .[]'); do
-    # echo "key is $key"
     RULE_NAME=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .ruleName')
-    # echo "ruleName is $RULE_NAME"
     REPO_NAME=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .repoName')
-    # echo "repoName is $REPO_NAME"
     DESTINATION_NAME=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .destinationRepo')
-    # echo "destinationRepo is $DESTINATION_NAME"
     TAG_VERSION=$(echo $RULES_CONFIG | jq -r '.rules | ."'$key'" | .tagVersion')
-    # echo "tagVersion is $TAG_VERSION"
 
     echo "Create ACR Cache"
     az acr cache create -r hmctspublic -n $RULE_NAME -s docker.io/$REPO_NAME -t $DESTINATION_NAME
