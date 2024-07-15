4bc416214f0fda297b148c5a22c1d8ca70eacb6b
Marty Fox <martin.fox1@justice.gov.uk>
2024-07-15 12:23:01 +0100

    Moving docker based images to caching rules, updating scripts to have better input validation and pipeline to use input names, adding caching credential to acr-cache.sh

--- base-image-scan.sh
+++ base-image-scan.sh
@@ -1,17 +1,73 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-targetRegistry=$1
-targetImage=$2
-baseTag=$3
-registryPublicUsername=$4
-registryPublicPassword=$5
-registrySlackWebhook=$6
-scannerUrl=$7
-scannerUsername=$8
-scannerPassword=$9
+targetRegistry=
+targetImage=
+baseTag=
+registryPublicUsername=
+registryPublicPassword=
+registrySlackWebhook=
+scannerUrl=
+scannerUsername=
+scannerPassword=
 
+usage(){
+>&2 cat << EOF
+------------------------------------------------
+Script to check if AKS cluster is active state
+------------------------------------------------
+Usage: $0
+    [ -tr | --targetRegistry ]
+    [ -ti | --targetImage ]
+    [ -bt | --baseTag ] 
+    [ -ru | --registryPublicUsername ]
+    [ -rp | --registryPublicPassword ]
+    [ -rw | --registrySlackWebhook ]
+    [ -su | --scannerUrl ]
+    [ -un | --scannerUsername ]
+    [ -sp | --scannerPassword ]
+    [ -h | --help ] 
+EOF
+exit 1
+}
+
+args=$(getopt -a -o tr:ti:bt:ru:rp:rw:su:un:sp: --long targetRegistry:,targetImage:,baseTag:,registryPublicUsername:,registryPublicPassword:,registrySlackWebhook:,scannerUrl:,scannerUsername:,scannerPassword:,help -- "$@")
+if [[ $? -gt 0 ]]; then
+    usage
+fi
+
+# Debug commands, uncomment if you are having issues
+# >&2 echo [$@] passed to script
+# >&2 echo getopt creates [${args}]
+
+eval set -- ${args}
+while :
+do
+    case $1 in
+        -h | --help)                    usage                     ; shift   ;;
+        -tr | --targetRegistry)         targetRegistry=$2         ; shift 2 ;;
+        -ti | --targetImage)            targetImage=$2            ; shift 2 ;;
+        -bt | --baseTag)                baseTag=$2                ; shift 2 ;;
+        -ru | --registryPublicUsername) registryPublicUsername=$2 ; shift 2 ;;
+        -rp | --registryPublicPassword) registryPublicPassword=$2 ; shift 2 ;;
+        -rw | --registrySlackWebhook)   registrySlackWebhook=$2   ; shift 2 ;;
+        -su | --scannerUrl)             scannerUrl=$2             ; shift 2 ;;
+        -un | --scannerUsername)        scannerUsername=$2        ; shift 2 ;;
+        -sp | --scannerPassword)        scannerPassword=$2        ; shift 2 ;;
+        --) shift; break ;;
+        *) >&2 echo Unsupported option: $1
+            usage ;;
+    esac
+done
+
+# Check if all arguments are provided
+if [ -z "$targetRegistry" ] || [ -z "$targetImage" ] || [ -z "$baseTag" ] || [ -z "$registryPublicUsername" ] || [ -z "$registryPublicPassword" ] || [ -z "$registrySlackWebhook" ] || [ -z "$scannerUrl" ] || [ -z "$scannerUsername" ] || [ -z "$scannerPassword" ]; then
+    echo "------------------------" 
+    echo 'Some values are missing, please supply all the required arguments' >&2
+    echo "------------------------"
+    exit 1
+fi
 
 echo "Getting auth token ..."
 _token=$(curl -s -k -H "Content-Type: application/json" -d "{\"password\": {\"username\": \"${scannerUsername}\", \"password\": \"${scannerPassword}\"}}" \
--- tag-new-image.sh
+++ tag-new-image.sh
@@ -1,13 +1,55 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
+set -euo pipefail
 
-baseTag=$1
-targetImage=$2
-acrName=$3
-targetRegistry=$4
-baseDigest=$5
-acrDigest=$6
+baseTag=
+targetImage=
+acrName=
+targetRegistry=
+baseDigest=
+acrDigest=
+
+usage(){
+>&2 cat << EOF
+------------------------------------------------
+Script to check if AKS cluster is active state
+------------------------------------------------
+Usage: $0
+    [ -bt |--baseTag ] 
+    [ -ti |--targetImage ]
+    [ -an |--acrName ]
+    [ -tr |--targetRegistry ]
+    [ -bd |--baseDigest ]
+    [ -ad |--acrDigest ]
+    [ -h |--help ] 
+EOF
+exit 1
+}
+
+eval set -- ${args}
+while :
+do
+    case $1 in
+        -h  | --help )           usage                  ; shift   ;;
+        -bt | --baseTag )        baseTag=$2             ; shift 2 ;;
+        -ti | --targetImage )    targetImage=$2         ; shift 2 ;;
+        -an | --acrName )        acrName=$2             ; shift 2 ;;
+        -tr | --targetRegistry ) targetRegistry=$2      ; shift 2 ;;
+        -bd | --baseDigest )     baseDigest=$2          ; shift 2 ;;
+        -ad | --acrDigest )      acrDigest=$2           ; shift 2 ;;
+        --) shift; break ;;
+        *) >&2 echo Unsupported option: $1
+        usage ;;
+    esac
+done
+
+# Check if all arguments are provided
+if [ -z "$baseTag" ] || [ -z "$targetImage" ] || [ -z "$acrName" ] || [ -z "$targetRegistry" ] || [ -z "$baseDigest" ] || [ -z "$acrDigest" ]; then
+    echo "------------------------" 
+    echo 'Some values are missing, please supply all the required arguments' >&2
+    echo "------------------------"
+    exit 1
+fi
 
 # Move base tag to new image
 [ "${acrDigest}" != "" ] && echo "Untagging previous ${baseTag} ..." && az acr repository untag -n ${acrName} --image ${targetImage}:${baseTag}
